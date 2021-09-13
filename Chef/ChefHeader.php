293f821c9b7769315249fa69c1e41537e62ef312
YongXian1202 <yongxiantong@gmail.com>
2021-09-13 15:18:31 +0800

    change header

--- Chef/ChefHeader.php
+++ Chef/ChefHeader.php
@@ -29,8 +29,7 @@ session_start();
       </li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
     </ul>
   </div>
 </nav>
