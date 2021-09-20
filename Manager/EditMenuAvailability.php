5b72b2d54d638a57286df4d2a92805831dd3a4fb
YongXian1202 <yongxiantong@gmail.com>
2021-09-20 23:13:34 +0800

    hi

--- Manager/EditMenuAvailability.php
+++ Manager/EditMenuAvailability.php
@@ -1,29 +1,210 @@
 <?php
-include "ManagerHeader.php";
+  include "ManagerHeader.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manager's Service</title>
-    
-    <style>
-    body{
-    background-image: url();
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-    background-color: #EDF6F9;
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu Availabilty</title>
+  <style>
+
+
+  body h1{
+    font-family:'Oleo Script';
+    font-size: 300%;
+    margin-top:10%;
+  }
+
+    .switch{
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
 }
 
-.img1{
-    min-height: 45%;
+/*Hide default HTML checkbox*/
+.switch input{
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider{
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+position: absolute;
+content: "";
+height: 26px;
+width: 26px;
+left: 4px;
+bottom: 4px;
+background-color: white;
+-webkit-transition: .4s;
+transition: .4s;
+}
+input:checked + .slider {
+background-color: #2196F3;
+}
+
+input:focus + .slider {
+box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+-webkit-transform: translateX(26px);
+-ms-transform: translateX(26px);
+transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+border-radius: 34px;
+}
+
+.slider.round:before {
+border-radius: 50%;
+}
+
+.categories{
+    padding-top:3vh;
+}
+
+.btn3{
+    width:5vw;
+    font-family: 'Oleo Script';
+    padding: 1rem 2.5rem;
+    background-color:#ffffff;
+    border-radius: 2rem;
+    margin: 1rem 0;
+    text-transform:uppercase;
+    color: #fdc094;
+    font-size: 1.4rem;
+    align-items: center;
+    align-content: center;
+}
+
+.btn3:hover{
+    transform: translateY(-2px);
+    box-shadow:.5rem .5rem 2rem rgba(0,0,0,.2)
+}
+
+.btn3:active{
+    transform:translateY(0);
+    box-shadow: none;
 }
 
 .text{
-    position:relative;
-    top:30%;
+    position:absolute;
+    top:50%;
+    width:100%;
+    text-align:center;
+    color:#000;
+    font-size:27px;
+    letter-spacing:8px;
+    text-transform: uppercase;
+}
+
+.text .border{
+    background-color:#120C6E;
+    color:#fff;
+    padding:20px;
+}
+
+.text .border.trans{
+    background-color:  transparent;
+}
+
+.img{
+    
+    min-height: 65%;
+}
+
+.img{
+    position: relative;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+
+.section{
+    text-align:center;
+    padding: 50px 80px;
+}
+
+.section-light{
+    background-color:#f4f4f4;
+    columns: #666;
+}
+
+input[type = "submit"]{
+    font-family: 'Oleo Script';
+    width:12vw;
+    padding: 1rem ;
+    background-color:#0B0742;
+    border-radius: 2rem;
+    margin: 1rem 0;
+    text-transform:uppercase;
+    color: #fdc094;
+    font-size: 1.4rem;
+    align-items: center;
+    align-content: center;
+}
+
+input[type = "submit"]:hover{
+    transform: translateY(-2px);
+    box-shadow:.5rem .5rem 2rem rgba(0,0,0,.2)
+	}
+
+input[type = "submit"]:active{
+    transform:translateY(0);
+    box-shadow: none;
+}
+
+input[type = "button"]{
+    font-family: 'Oleo Script';
+    width:12vw;
+    padding: 1rem 2.5rem;
+    background-color:#0B0742;
+    border-radius: 2rem;
+    margin: 1rem 0;
+    text-transform:uppercase;
+    color: #fdc094;
+    font-size: 1.4rem;
+    align-items: center;
+    align-content: center;
+}
+
+input[type = "button"]:hover{
+    transform: translateY(-2px);
+    box-shadow:.5rem .5rem 2rem rgba(0,0,0,.2)
+	}
+
+input[type = "button"]:active{
+    transform:translateY(0);
+    box-shadow: none;
+}
+    
+
+
+a{
+    text-decoration: none;
+}
+
+.text{
+    position:absolute;
+    padding-top:5vh;
     width:100%;
     text-align:center;
     color:#000;
@@ -31,19 +212,19 @@ include "ManagerHeader.php";
     letter-spacing:8px;
     text-transform: uppercase;
     font-family: 'Oleo Script';
-    padding:50px;
-
 }
 
 .text .border{
-    background-color:#EDF6F9;
+    background-color: #EDF6F9;;
+    color:#000000;
     padding:20px;
-    
+
 }
 
 .text .border.trans{
     background-color:  transparent;
 }
+
 
 .card-wrapper{
     display:flex;
@@ -62,6 +243,7 @@ include "ManagerHeader.php";
     margin:2rem 0;
     box-shadow: .5rem .5rem 3rem rgba(0,0,0,0.2);
     font-family: 'Oleo Script';
+    padding-bottom:2vw;
 }
 
 .card .card-img{
@@ -76,6 +258,7 @@ include "ManagerHeader.php";
     font-size:2.5rem;
     color: #333;
     margin:1.5rem 0;
+    font-family:'Oleo Script';
 }
 
 .about{
@@ -90,41 +273,22 @@ include "ManagerHeader.php";
 
 .card .btn{
     padding: 1rem 2.5rem;
-    background-color:#0B0742;
+    background-color:green;
     border-radius: 2rem;
     margin: 1rem 0;
     text-transform:uppercase;
-    color: #fdc094;
+    color: white;
     font-size: 1.4rem;
 }
 
-.card .popuptext {
-    visibility: hidden;
-    width: 16vw;
-    background-color: #0066ff;
-    color: #fff;
-    text-align: center;
-    border-radius: 6px;
-    padding: 8px 0;
-    position: absolute;
-    z-index: 1;
-    bottom: 120%;
-    left: 50%;
-    margin-left: -8vw;
-}
-
-.card .show {
-    visibility: visible;
-  }
-
-.card .btn:hover{
-    transform: translateY(-2px);
-    box-shadow:.5rem .5rem 2rem rgba(0,0,0,.2)
-}
-
-.card .btn:active{
-    transform:translateY(0);
-    box-shadow: none;
+.card .btn2{
+    padding: 1rem 2.5rem;
+    background-color:red;
+    border-radius: 2rem;
+    margin: 1rem 0;
+    text-transform:uppercase;
+    color: white;
+    font-size: 1.4rem;
 }
 
 @media screen and (min-width:700px){
@@ -149,25 +313,37 @@ include "ManagerHeader.php";
     }
 }
     
-.card:nth-child(1){
-        animation:fadeIn .5s 0.5s backwards;
+    .card:nth-child(1){
+        animation:fadeIn .5s 1s backwards;
     }
 
     .card:nth-child(2){
-        animation:fadeIn .5s 0.5s backwards;
+        animation:fadeIn .5s 1s backwards;
     }
 
     .card:nth-child(3){
-        animation:fadeIn .5s 0.5s backwards;
+        animation:fadeIn .5s 1s backwards;
     }
     .card:nth-child(4){
-        animation:fadeIn .5s 0.5s backwards;
+        animation:fadeIn .5s 1s backwards;
     }
     .card:nth-child(5){
-        animation:fadeIn .5s 0.5s backwards;
+        animation:fadeIn .5s 1s backwards;
     }
     .card:nth-child(6){
-        animation:fadeIn .5s 0.5s backwards;
+        animation:fadeIn .5s 1s backwards;
+    }
+    .card:nth-child(7){
+        animation:fadeIn .5s 1s backwards;
+    }
+    .card:nth-child(8){
+        animation:fadeIn .5s 1s backwards;
+    }
+    .card:nth-child(9){
+        animation:fadeIn .5s 1s backwards;
+    }
+    .card:nth-child(10){
+        animation:fadeIn .5s 1s backwards;
     }
 
     .card:hover{
@@ -175,313 +351,72 @@ include "ManagerHeader.php";
     }
 }
 
-a{
-    text-decoration:none;
-}
-
-.container {  
-    width:20vw;
-    font-family: 'Oleo Script';
-    padding: 1.5rem 2.5rem;
-    background-color:#0B0742;
-    border-radius: 2rem;
-    margin: 1rem 0;
-    text-transform:uppercase;
-    font-size: 1.5rem;
-    align-items: center;
-    align-content: center;
-}  
-
-button {  
-    background-color: lightblue;  
-    color: black;  
-
-}  
-
-/* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-
-
-
-</style>
+  </style>
 </head>
-<body>
-<div class="img1">
-<div class="text">
+<body style="background-color:#EDF6F9;">
+  
+  <div class="img">
+  
+    <div class="text">
         <span class="border">
-            Edit Menu Availability
+            MENU AVAILABILITY
         </span>
-</div>
-</div>
-
-<!-- <label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> -->
-
-
-<div class="img1">
-<div class="text">
-        <span class="border">
-            Main Cousine
-        </span>
-</div>
-</div>
-<section>
-    <div class="card-wrapper">
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-
- 
-
     </div>
-    </section>
-    
-
-<div class="img1">
-<div class="text">
-        <span class="border">
-            Dessert
-        </span>
-</div>
 </div>
 
-    <section>
-    <div class="card-wrapper">
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
+<section class="bg" style="background-color:#EDF6F9;">
+  <center>
+  <form action="UpdateAvailability.php" method="post">
+      <?php
 
- 
+      include "connectdb.php";
 
-    </div>
-    </section>
+      $sql = "SELECT Item_ID, Item_Name, Availability, Image FROM item WHERE Main_ID = 1;";
+      $result = mysqli_query($conn, $sql);
 
+      if (mysqli_num_rows($result) <= 0) {
+          echo "<script>alert('There are no food.');</script>";
+      } 
+      else {
+        echo"
+        <h1>Main Course</h1>
+        <div class='menu'>
+        <section>
+        <div class='card-wrapper'>";
+   
+          while ($rows = mysqli_fetch_array($result)) {
+              $item_id = $rows['Item_ID'];
+              $item_name = $rows['Item_Name'];
+              $availability = $rows['Availability'];
+              $image = $rows['Image'];
+              ?>
+            <div class='card'>
+              <img width='200vw' height='160vw' src="\FYP\Manager/<?php echo $image; ?>" alt="card backgroud" class="card-img">
+                  <h1><?php echo $item_name; ?></h1></br>
+                    <label class="switch">
+                      <input type ="checkbox" name ="item_id[]" value="<?php echo $item_id.',';?>" <?php if($availability == "Available"){echo "checked";}?>>
+                      <span class = "slider round"></span>
+                    </label>
+          </br>
+          </div>                    
+                                  
+                                      <?php
+                    
+                                    }
+                                    echo"</div>
+                                    </section>
+                                    </div>"; }
+                ?> 
 
-    <div class="img1">
-<div class="text">
-        <span class="border">
-            Drinks
-        </span>
-</div>
-</div>
-
-    <section>
-    <div class="card-wrapper">
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-            <div class="card">
-                <img src="../image/editmenu.jfif" alt="card backgroud" class="card-img">
-                <h1>Chicken Rice</h1>
-                <label class="switch" style="margin-bottom: 15px;">
-                <input type="checkbox">
-                <span class="slider round"></span>
-                </label>
-            </div>
-
- 
-
-    </div>
-    </section>
-
-    <center>
-    <div class="container" style="margin-bottom: 15px; width:5vw;">
-    <a href="#"><div class="button" style="color:#fdc094">Save</div></a>
+<table>
+    <tr rowspan="2">
+    <td><center><input type="submit" value="Save Changes" class="saveBtn"></a></center></td>
+    <td><center><a href="ChefHome.php"><input type="button" value="Back" class="btn3"></a></center></td>
+    </tr>
+    </table>
+    </form>
     </center>
-
-    <center>
-    <div class="container" style="margin-bottom: 20px;">
-    <a href="EditMenu.php"><div class="button" style="color:#fdc094">Return To The Previous Page</div></a>
-    </center>
-
+</section>
 </body>
+
 </html>
