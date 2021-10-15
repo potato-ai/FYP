f11b36ec3f2c65aa4c2b38ecd7a471f74fb791fe
YongXian1202 <yongxiantong@gmail.com>
2021-10-15 22:47:56 +0800

    added session

--- Manager/EditMenuDetails.php
+++ Manager/EditMenuDetails.php
@@ -1,5 +1,15 @@
 <?php
 include "ManagerHeader.php";
+include "connectdb.php";
+if(isset($_SESSION['User_ID'])) {
+    $sql = "SELECT Name FROM user WHERE User_ID = '{$_SESSION['User_ID']}'";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        $row['Name'];
+        
+        
+    }
 ?>
 
 <!DOCTYPE html>
@@ -522,3 +532,10 @@ photo.addEventListener("change", function(){
 
 </body>
 </html>
+<?php
+}
+else{
+    header("Location: login.php");
+    exit();
+}
+?>
